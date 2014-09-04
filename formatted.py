06bb44d3df3637d0a38fadf1a9cd95cc83b6403d
Tollevin Williams <tollevin.williams@spsmail.cuny.edu>
2014-09-03 22:11:23 -0400

    Woof

--- formatted.py
+++ formatted.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 """Provides variables for formatting."""
 
-NEWS = 'Hi {friend}! I have {0} news! I won the raffle with number {1:.4f}!'
+NEWS = 'Hi {friend}! I have {0} news! I won the raffle with number {1:0>6}!'
 FNAME = 'Pat'
 NTYPE = '*amazing*'
 RNUM = 42
