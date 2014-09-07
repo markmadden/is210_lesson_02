7a640630e6d7d4bb1e00a218c8bd740806dbd394
Mark Madden <mark.madden@spsmail.cuny.edu>
2014-09-07 13:25:19 -0400

    'task06'

--- artists.py
+++ artists.py
@@ -7,3 +7,5 @@ THE_GREAT_QUESTION = ('Michaelangelo. Leonardo. Rafael. Donatello. Turtles? '
                       'Creators of the great works? Both? You be the judge!')
 STATEMENTS = THE_GREAT_QUESTION.split(". ")	
 ARTISTS = STATEMENTS[:4]
+NUM_ARTISTS = len(ARTISTS)
+CHARACTERS = len(THE_GREAT_QUESTION)
