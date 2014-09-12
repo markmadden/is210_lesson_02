a89d1539fe15b8224840f5b94ffffd0ff2f038b1
mark madden <mark.madden@spsmail.cuny.edu>
2014-09-12 14:48:07 -0400

    finally conquired task 19

--- formatted.py
+++ formatted.py
@@ -2,9 +2,19 @@
 # -*- coding: utf-8 -*-
 """Provides variables for formatting."""
 
-NEWS = 'Hi {friend}! I have {0} news! I won the raffle with number {1}!'
-FNAME = 'Pat'
-NTYPE = '*amazing*'
-RNUM = 42
+FNAME = 'Chad'
+NTYPE = 'great'
+RNUM = 210
+NEWS = 'Hi {friend}! I have {0} news! I won the raffle with number {1:0>6d}!'
 
-NEWS = NEWS.format(a,b,%06d)
+EMAIL = NEWS.format(NTYPE, RNUM, friend=FNAME)
+
+'''
+IGNORE SCRATCHWORK BELOW
+
+EMAIL = NEWS.format("great", 210, friend="Chad")
+EMAIL = NEWS.format(friend=FNAME, NTYPE, RNUM)
+scratch work
+
+
+NEWS = NEWS.format(a,b,%06d)'''
