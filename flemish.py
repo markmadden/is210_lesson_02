b4fb8e34b30c220990f9b78de252851dcaa7f6de
mark madden <mark.madden@spsmail.cuny.edu>
2014-09-12 14:48:07 -0400

    changing flemish.py

--- flemish.py
+++ flemish.py
@@ -5,5 +5,12 @@
 
 import inquisition
 FISHY = inquisition.SPANISH.replace('surprise', 'haddock')
-VAR1 = "Spanish"
-VAR1L = len(VAR1)
+IND = FISHY.index("Spanish")
+VAR1L = len("Spanish")
+FLEMISH = FISHY[:19] + "Flemish" + FISHY[26:]
+
+"""
+ignore scratchwork below
+a , b, c, d, e = FISHY.split(' ',4)
+d = "Flemish"
+FISHY = a + b + c + d + e"""
